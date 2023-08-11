f1cefccbaeac89c81fced9641ef0ee29be4efaea
Jiří Zubryckyj <64204363+skokann@users.noreply.github.com>
2023-08-11 13:33:54 +0200

    added Bussines section, card for Business section, texts.

--- bank-app/src/App.jsx
+++ bank-app/src/App.jsx
@@ -21,12 +21,12 @@ const App = () => (
       </div>
     </div>
 
-    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+    <div className={`bg-primary ${styles.flexStart}`}>
       <div className={` ${styles.boxWidth}`}>
         <Hero />
       </div>
     </div>
-    <div className={`bg-primary ${styles.flexStart}`}>
+    <div className={`bg-primary ${styles.paddingX}  ${styles.flexStart}`}>
       <div className={` ${styles.boxWidth}`}>
         <Stats />
         <Business /> <Billing /> <CardDeal /> <Testimonials /> <Clients />{" "}
